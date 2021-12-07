f0c1d7218c0ed39b51f3e4ec45c06da55e479475
Swiiptz <milan.etudes@gmail.com>
2021-12-07 17:24:09 +0100

    Maj

--- Test1.cpp
+++ Test1.cpp
@@ -1,9 +1,14 @@
 #include <iostream> //charge la fonctionnalité bibliothèque
- 
+
 using namespace std;
- 
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int nbEnfants(-1);
+    while (nbEnfants < 0)
+    {
+        cout << "Vous n'avez pas d'enfant" << endl;
+        cin >> nbEnfants;
+    }
+    cout << "Oh vous avez des enfants" << endl;
 }
