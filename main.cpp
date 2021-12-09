af58c1f73ab9fd03a7e5f26d003e253a345c29c7
Swiiptz <milan.etudes@gmail.com>
2021-12-09 20:19:59 +0100

    Maj

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int const tailleTableauHigh(5);
+    int meilleurScore[tailleTableauHigh];       //Déclare un tableau de 5 int 
+    int const tailleTableauAngle(3);
+    double anglesTriangle[tailleTableauAngle];   //Déclare un tableau de 3 double
+
+   return 0;
+}
