614b8790dc435e9387df65897ff15c8587d944ae
Swiiptz <milan.etudes@gmail.com>
2021-12-09 21:04:46 +0100

    Programme calculs moyennes Bests

--- main.cpp
+++ main.cpp
@@ -3,10 +3,31 @@ using namespace std;
 
 int main()
 {
-    int const tailleTableauHigh(5);
-    int meilleurScore[tailleTableauHigh];       //Déclare un tableau de 5 int 
-    int const tailleTableauAngle(3);
-    double anglesTriangle[tailleTableauAngle];   //Déclare un tableau de 3 double
-
+    int input;
+    double tempNotes;
+    cout << "Nombre de note" << endl;
+    cin >> input;
+    int const nombreNotes(input);
+    double notes[nombreNotes];
+    for (int i(0); i < nombreNotes; i+=1)
+    {
+        cout << "Note" << i+1 << ": " << endl;
+        cin >> tempNotes;
+        notes[i] = tempNotes;
+    }
+    double moyenne(0);
+    for(int i(0); i<nombreNotes; ++i)
+    {
+        moyenne += notes[i];   //On additionne toutes les notes
+    }
+    moyenne /= nombreNotes;
+    if (moyenne < 10)
+    {
+        cout << "Moyenne nul ! Cheh, retourne en 6eme NOOB, ta moyenne pas ouf :" << moyenne <<endl;
+    }
+    else
+    {
+        cout << "Votre moyenne est de :" << moyenne << endl;
+    }
    return 0;
 }
